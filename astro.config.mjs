9ec74770c31a83af219abb34e5f8eff1df07d164
Michele Sanfilippo <michelesanfilippo01@gmail.com>
2025-05-21 09:51:06 +0000

    fix route

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sgogov.dev",
+  site: "https://michelesanfilippo.github.io",
   integrations: [react(), sitemap()],
 });
