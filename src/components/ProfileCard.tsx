813b03877276737c7ff6085ee26791fb142cb814
Michele Sanfilippo <michelesanfilippo01@gmail.com>
2025-11-27 13:32:15 +0000

    fix avatar

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -16,9 +16,9 @@ const round = (v: number, precision = 3) => parseFloat(v.toFixed(precision));
 const adjust = (v: number, fMin: number, fMax: number, tMin: number, tMax: number) => round(tMin + ((tMax - tMin) * (v - fMin)) / (fMax - fMin));
 
 const ProfileCardComponent = ({
-  avatarUrl = '/avatar/MS.jpg',
+  avatarUrl = '/avatar/download.png',
   iconUrl = '/avatar/dev.png',
-  grainUrl = '/avatar/grain01.jpg',
+  grainUrl = '',
   innerGradient,
   behindGlowEnabled = true,
   behindGlowColor,
